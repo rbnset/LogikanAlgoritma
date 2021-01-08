6970b08372ec1285483df234d1e0133cd58124a7
rbnsetiyawan <rbn.setiyawan@gmail.com>
2021-01-08 08:35:37 +0700

    Latihan var2

---
+++ P2_Penulisan Program/Lat_Program4.pas
@@ -0,0 +1,17 @@
+program Hitung3;
+uses crt;
+var
+	a,b: Integer;
+	c: Real;
+begin
+	a:=10;
+	b:=20;
+	{write('Bilangan a: ');readln(a);
+	write('Bilangan b: ');readln(b);}
+	c:=a/b;
+	writeln('Nilai A : ',A);
+	writeln('Nilai B : ',B);
+	writeln('____________');
+	writeln;
+	write('Pembagian Bilangan ',a,'/',b,'=',c);
+end.

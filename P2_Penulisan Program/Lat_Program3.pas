5d5a58de4f883ca6f887af37f99d57ae115bc51c
rbnsetiyawan <rbn.setiyawan@gmail.com>
2021-01-08 08:29:30 +0700

    Latihan var

---
+++ P2_Penulisan Program/Lat_Program3.pas
@@ -0,0 +1,17 @@
+program Hitung2;
+uses crt;
+var
+	a,b,c: Integer;
+begin
+	a:=10;
+	b:=20;
+	// ini adalah komentar di dalam komentar perintah tidak akan di jalankan
+	{ write ('Bilangan a: ');readln(a);
+	write('Bilangan b: ');readln(b);}
+	c:=a+b;
+	writeln('Nilai A := ',A);
+	writeln('Nilai B := ',B);
+	writeln('_____________');
+	writeln;
+	write('Penjumlahan Bilangan ',a,'+',b,'=',c);
+end.

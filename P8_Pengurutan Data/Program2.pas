a9c4cb48add6ae7bbc6fc909b25ad44e3c852282
rbnsetiyawan <rbn.setiyawan@gmail.com>
2021-01-19 12:36:41 +0700

    finish Program2

---
+++ P8_Pengurutan Data/Program2.pas
@@ -0,0 +1,50 @@
+Program Selection_sort_Descending;
+uses crt;
+
+const   
+maks = 50;
+
+type
+Larik = array[1..maks] of integer;
+
+var
+n,i,j : integer;
+L: larik;
+min    : integer;
+temp     : integer;
+
+{=======================INPUT LARIK======================}
+begin
+writeln('Masukkan Jumlah data : '); readln(n);
+for i:= 1 to n do
+begin
+    writeln('Masukkan data ke-',i,' : ');read(L[i]);
+end;
+    writeln('==================');
+    writeln('Data input nya adalah : ');
+for i:=1 to n  do
+    write(L[i],' ');
+
+{============PROSES SORTING SELECTION SORT DESCENDING========================}
+For i:=1 to n-1 do
+begin
+Min:=i;
+For j:= i to n do
+begin
+            If L[j] > L[min] then
+            Min:=j;
+End;
+Temp:=L[i];
+    L[i]:=L[min];
+    L[min]:=temp;
+End;
+{===============TAMPIL DATA=========================}
+
+writeln;
+writeln('Data setelah Diurutkan dengan Selection Sort Descending : ');
+for i := 1 to n do
+write(L[i],' ');
+readln;
+
+readln;
+end.

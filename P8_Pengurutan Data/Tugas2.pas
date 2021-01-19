e9127ce152a8b98b17787e296790f614e0065860
rbnsetiyawan <rbn.setiyawan@gmail.com>
2021-01-19 14:01:50 +0700

    Program2

--- P8_Pengurutan Data/Tugas2.pas
+++ P8_Pengurutan Data/Tugas2.pas
@@ -1,56 +1,93 @@
-Program Tugas2_SelectionSort;
+program Tugas2_SelctionSort;
 uses crt;
+var 
+pil:string[1];
 
-const   
-maks = 50;
-
-type
-Larik = array[1..maks] of integer;
-
-var
-n,i,j 	: integer;
-L 		: larik;
-min    	: integer;
-temp    : integer;
-
-{=======================INPUT LARIK======================}
+Procedure Ascending;
+var a:array [1..100] of integer;
+x,i,j, posisi, penampung:integer;
 begin
-writeln('Masukkan Jumlah data : '); readln(n);
-for i:= 1 to n do
+	writeln('*** ANDA MENGGUNAKAN PROGRAM SELECTION SORT ASCENDING ***');
+	writeln('Tekan ENTER untuk setiap data)');
+for i:=1 to x do
 begin
-    writeln('Masukkan data ke-',i,' : ');read(L[i]);
+	write('Masukkan angka ke-',i,':'); readln(a[i]);
 end;
-    writeln('==================');
-    writeln('Data input nya adalah : ');
-for i:=1 to n  do
-    write(L[i],' ');
 
-{============PROSES SORTING SELECTION SORT DESCENDING========================}
-For i:=1 to n-1 do
+for i:=1 to x do
 begin
-Min:=i;
-For j:= i to n do
+posisi:=i;
+for j:=i+1 to x do
 begin
-            If L[j] > L[min] then
-            Min:=j;
-End;
-Temp:=L[i];
-    L[i]:=L[min];
-    L[min]:=temp;
-End;
-{===============TAMPIL DATA=========================}
-
+if a[posisi]>a[j] then
+posisi:=j;
+end;
+penampung:=a[i];
+a[i]:=a[posisi];
+a[posisi]:=penampung;
+end;
 writeln;
-writeln('Data setelah Diurutkan dengan Selection Sort Ascending : ');
-for i := 1 to n do
-write(L[i],' ');
-readln;
-readln;
+write('Hasil Selection Sort Ascending' );
 writeln;
-writeln('Data setelah Diurutkan dengan Selection Sort Descending : ');
-for i := 1 to n do
-write(L[i],' ');
+for j:=1 to x do
+begin
+write('Urutan ke-',j,':'); write(a[j]);
+writeln;
+end;
 readln;
+end;
 
+Procedure Discending;
+var a:array [1..100] of integer;
+x,i,j, posisi, penampung:integer;
+begin
+writeln('*** ANDA MENGGUNAKAN PROGRAM SELECTION SORT DISCENDING ***');
+write('Masukkan banyak angka: '); readln(x);
+writeln('(Tekan ENTER untuk setiap data)');
+for i:=1 to x do
+begin
+write('Masukkan angka ke-',i,':'); readln(a[i]);
+end;
+
+for i:=1 to x do
+begin
+posisi:=i;
+for j:=i+1 to x do
+begin
+if a[posisi]<a[j] then
+posisi:=j;
+end;
+penampung:=a[i];
+a[i]:=a[posisi];
+a[posisi]:=penampung;
+end;
+writeln;
+write('Hasil Selection Sort Discending ' );
+writeln;
+for j:=1 to x do
+begin
+write('Urutan ke-',j,':'); write(a[j]);
+writeln;
+end;
 readln;
+end;
+
+{program utama}
+begin
+clrscr;
+writeln('PROGRAM SELECTION SORT');
+writeln('Nama : Robin Setiyawan');
+writeln('Nim / Prodi: 20230027 / Sistem Informasi');
+repeat
+writeln;
+writeln('                 ==========|| MENU UTAMA ||=========');
+writeln('                 || 1. Selection Sort Ascending   ||');
+writeln('                 || 2. Selection Sort Discending  ||');
+writeln('                 || 3. EXIT                       ||');
+writeln('                 ====================================');
+write('                   Masukkan Pilihan 1/2/3: ');readln(pil);
+
+if pil='1' then Ascending
+else if pil='2' then Discending;
+until pil='3';
 end.
